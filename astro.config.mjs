7752f60dfd32e43351426e92f339a6f6c8a5f9ea
annsann <git@annsann.eu>
2024-09-09 15:37:05 +0200

    Docs: Move new doc within the doc folder

---
+++ astro.config.mjs
@@ -0,0 +1,48 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import { rehypeGithubAlerts } from 'rehype-github-alerts';
+import starlightImageZoom from 'starlight-image-zoom';
+
+// https://astro.build/config
+export default defineConfig({
+    markdown: {
+        rehypePlugins: [rehypeGithubAlerts],
+    },
+	integrations: [
+		starlight({
+			title: 'Riot Guides',
+			social: {
+				github: 'https://github.com/riot-os/riot',
+			},
+			sidebar: [
+                {
+                    label: 'General',
+                    items: [
+                        { label: 'Introduction', slug: 'index'},
+                        { label: 'Structure', slug: 'general/structure'},
+                        { label: 'Quick Start', slug: 'general/quick-start'},
+                    ],
+                },
+                {
+                    label: 'Getting Started',
+                    items: [
+                        // We need to specify the order of the items here
+                        // to make it easier to follow the guide
+                        { slug: "getting-started/install-wsl" },
+                        { slug: "getting-started/installing"},
+                        { slug: "getting-started/flashing"},
+                    ]
+                },
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },  
+				},
+			],
+            customCss: [
+                './src/styles/github_alerts.css',
+            ],
+            plugins: [starlightImageZoom()]
+		}),
+	],
+});
